7a383a9e1fce57a0289caceaf109edd8e90493cd
alicebelchior <alice.belchior23@gmail.com>
2023-10-06 23:40:52 -0300

    06 - Entrada de Dados

--- PRATICA/JavaScript - Fundamentos - Prática.js
+++ PRATICA/JavaScript - Fundamentos - Prática.js
@@ -1,5 +1,11 @@
-var salBruto = 7500;
+// definir uma constante que vai importar um módulo p/ entrada de dados
+const prompt = require("prompt-sync")();
+
+var nome = prompt("Digite o seu nome: ");
+
+// a var abaixo se nao tivesse o "parseFloat" iria ler a entrada de dado como uma string
+var salBruto = parseFloat(prompt("Digite o valor do seu salário bruto: "));
 var ir = 0.10 * salBruto;
 var salLiq = salBruto - ir;
 
-console.log("O salário líquido é R$", salLiq)
+console.log("O salário líquido é R$", salLiq);
--- PRATICA/README.md
+++ PRATICA/README.md
@@ -1,6 +1,7 @@
 Problema
 
-Elabore um algoritmo para calcular o salário líquido de uma pessoa, sabendo que seu salário bruto é de R$7.500,00 e o imposto de renda a ser descontado é equivalente a 10% do salário bruto.
+Elabore um algoritmo para calcular o salário líquido de uma pessoa, solicitando seu nome e seu salário bruto. 
+O imposto de renda a ser descontado é equivalente a 10% do salário bruto.
 Ao final, mostre o valor do salário líquido.
 
-05 - Operadores.
+06 - Entrada de Dados
