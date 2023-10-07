d62ef103fe1c223eace31b635ce45bcfed6f0e8d
alicebelchior <alice.belchior23@gmail.com>
2023-10-07 14:24:33 -0300

    08 - Comando de Decisão Aninhados

--- PRATICA/README.md
+++ PRATICA/README.md
@@ -1,10 +1,14 @@
 Problema
 
 Elabore um algoritmo para calcular o salário líquido de uma pessoa, solicitando seu nome e seu salário bruto.
+Peça também, para o usuário digitar a quantidade de dependentes.
+Calcule a renda familiar per capta. Caso a renda para cada membro da família seja menor que R$500,00 
+a pessoa ficará isenta de IR, ou seja, não será calculado dentre as faixas salariais e seu valor será zero.
+Caso a renda para cada membro da família seja maior ou igual a R$500,00,
 O imposto de renda a ser descontado deve considerar as seguintes regras:
 * salario bruto ate R$1903,98 - 5%
 * salario bruto de R$1903,98 ate R$2826,65 - 7,5%
 * salario bruto a partir de R$2826,65 - 15%
 Ao final, mostre o valor do salário líquido.
 
-07 - Comandos de Decisão
+08 - Comando de Decisão Aninhados
--- PRATICA/pratica.js
+++ PRATICA/pratica.js
@@ -2,17 +2,24 @@
 const prompt = require("prompt-sync")();
 
 var nome = prompt("Digite o seu nome: ");
-
-// a var abaixo se nao tivesse o "parseFloat" iria ler a entrada de dado como uma string
 var salBruto = parseFloat(prompt("Digite o valor do seu salário bruto: "));
+var dep = parseInt(prompt("Digite o número de dependentes: "));
 
-if (salBruto > 0.00 && salBruto <= 1903,98) {
-    var ir = 0.05 * salBruto;
-} else if (salBruto > 2826,65) {
-    var ir = 0.15 * salBruto;
+//renda per capta = salario / pessoas numa casa (dependentes e a pessoa que trabalha)
+var renda = salBruto / (dep + 1);
+
+if (renda >= 500) {
+    if (salBruto > 0.00 && salBruto <= 1903,98) {
+        var ir = 0.05 * salBruto;
+    } else if (salBruto > 2826,65) {
+        var ir = 0.15 * salBruto;
+    } else {
+        var ir = 0.075 * salBruto;
+    }
 } else {
-    var ir = 0.075 * salBruto;
+    var ir = 0;
 }
+
 var salLiq = salBruto - ir;
 
 console.log("O salário líquido é R$", salLiq);
